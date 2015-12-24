77f22abc729ace21da54b21ac62813771af85bc5
Kun <liukun1016@gmail.com>
2015-12-23 21:09:46 -0500

    Update vlad_process.py

--- data_processing/vlad_process.py
+++ data_processing/vlad_process.py
@@ -2,8 +2,8 @@ from scipy.cluster.vq import vq, kmeans
 from numpy import zeros, subtract, add, ndarray, savetxt, asarray, append
 from utility import read_feature_vector, list_all_files
 from window_process import get_win_label
-from sklearn.cluster import MiniBatchKMeans
-# from kmeans.kmeans import mykmeans_plus
+#from sklearn.cluster import MiniBatchKMeans
+from kmeans.kmeans import mykmeans_plus
 
 def learn_vocabulary(k, max_iter, single_file, vector_matrix=None, input_path=None):
     if vector_matrix is None:
@@ -16,11 +16,11 @@ def learn_vocabulary(k, max_iter, single_file, vector_matrix=None, input_path=No
                 vector = read_feature_vector(input_path + file_names[f])
                 if vector is not None:
                     vector_matrix = append(vector_matrix, vector, axis=0)
-    # return mykmeans_plus(data=vector_matrix,k=k, max_iter=max_iter)[0]
+    return mykmeans_plus(data=vector_matrix,k=k, max_iter=max_iter)[0]
     #return kmeans(obs=vector_matrix, k_or_guess=k, iter=max_iter)[0]
-    km = MiniBatchKMeans(max_iter=max_iter, n_clusters=k)
-    km.fit(X=vector_matrix)
-    return km.cluster_centers_
+    #km = MiniBatchKMeans(max_iter=max_iter, n_clusters=k)
+    #km.fit(X=vector_matrix)
+    #return km.cluster_centers_
     
 def vlad_vector(vector_path, vocabulary):
     features = read_feature_vector(vector_path)
